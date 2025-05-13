68787c5aa80dc6007e1de188fe665bae15a7b86c
Evan <evan.rktcisse@gmail.com>
2025-05-14 01:04:50 +0200

    added Health component for damage and death handling

---
+++ Assets/Scenes/Scripts/Health.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+
+    public UnityEvent onDeath;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        if (onDeath == null)
+            onDeath = new UnityEvent();
+    }
+
+    public void SetMaxHealth(float amount)
+    {
+        maxHealth = Mathf.Max(0f, amount);
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth = Mathf.Max(0f, currentHealth - damage);
+        
+        if (currentHealth <= 0f)
+        {
+            onDeath.Invoke();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth = Mathf.Min(maxHealth, currentHealth + amount);
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    private void OnValidate()
+    {
+        maxHealth = Mathf.Max(0f, maxHealth);
+        currentHealth = Mathf.Max(0f, currentHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
